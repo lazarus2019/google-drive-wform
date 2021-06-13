2ec6ee007ab6f16ba8f606cc4adbbb5eb5fa101b
NTSon <thaisonbk2020@gmail.com>
2021-06-13 22:22:32 +0700

    Create and save token GGDrive, request basic with folders and files.

--- Form1.cs
+++ Form1.cs
@@ -20,7 +20,7 @@ namespace Google_Drive
 {
 	public partial class Form : System.Windows.Forms.Form
 	{
-
+		#region Declare valriables
 		// If modifying these scopes, delete your previously saved credentials
 		// at ~/.credentials/drive-dotnet-quickstart.json
 		static string[] Scopes = { DriveService.Scope.Drive };
@@ -30,17 +30,84 @@ namespace Google_Drive
 		// Function
 		private List<string> listFilePathUpload = new List<string>();
 
+		#endregion
+
+		#region Default form
 		public Form()
 		{
 			InitializeComponent();
 		}
-
 
 		private void Form_Load(object sender, EventArgs e)
 		{
 			progressBar.Minimum = 0;
 			progressBar.Value = 0;
 		}
+
+		#endregion
+
+		#region Create token and basic request
+
+		public void CreateService()
+		{
+			UserCredential cre;
+			var credPath = "token.json";
+
+			using (var stream = new System.IO.FileStream("ggdrive_client_secret.json", System.IO.FileMode.Open, System.IO.FileAccess.Read))
+			{
+				cre = GoogleWebAuthorizationBroker.AuthorizeAsync(
+					GoogleClientSecrets.Load(stream).Secrets,
+					Scopes,
+					"user",
+					CancellationToken.None,
+					new FileDataStore(credPath, true)).Result;
+				Console.WriteLine("Credential file saved to: " + credPath);
+
+				// Create Drive API service.
+				Service = new DriveService(new BaseClientService.Initializer()
+				{
+					HttpClientInitializer = cre,
+					ApplicationName = AppName,
+				});
+			}
+		}
+
+		private void btnRequest_Click(object sender, EventArgs e)
+		{
+			if (Service.ApplicationName != AppName)
+			{
+				CreateService();
+			}
+			ListFilesandFodlers();
+		}
+
+		#endregion
+
+		#region Files and folder
+		
+		public void ListFilesandFodlers()
+		{
+			if (Service.ApplicationName != AppName)
+			{
+				CreateService();
+			}
+
+			var downloader = new MediaDownloader(Service);
+
+			var request = Service.Files.List();
+
+			request.Q = inputRequest.Text;
+
+			var results = request.Execute();
+
+			foreach (var result in results.Files)
+			{
+				Debug.WriteLine($"{result.Name} | {result.Id}");
+			}
+		}
+		
+		#endregion
+
 
 	}
 }
